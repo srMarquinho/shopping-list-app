f18149cd298b7632c4d66df6b86ee3f3837b59b5
paulkenrick <pkenrick@gmail.com>
2016-07-28 15:15:05 +0100

    Fixes associations

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,9 +1,9 @@
-def signup(email: 'test@example.com', password: 'testtest', password_confirmation: 'testtest')
+def signup(email: 'test1@example.com', password: 'testtest', password_confirmation: 'testtest')
   visit('/')
   click_link('Sign up')
-  fill_in('Email', with: 'test@example.com')
-  fill_in('Password', with: 'testtest')
-  fill_in('Password confirmation', with: 'testtest')
+  fill_in('Email', with: email)
+  fill_in('Password', with: password)
+  fill_in('Password confirmation', with: password_confirmation)
   click_button('Sign up')
 end
 
@@ -15,4 +15,10 @@ def signin
   fill_in('Email', with: 'test@example.com')
   fill_in('Password', with: 'testtest')
   click_button('Log in')
+end
+
+def add_item(name: 'Shampoo')
+  click_link 'Add an item'
+  fill_in 'Name', with: name
+  click_button 'Create Item'
 end
