f3b6b47d6a23707fb23635d54514413b3e40c216
paulkenrick <pkenrick@gmail.com>
2016-08-02 11:47:30 +0100

    Alert is connected to distance checker, and shows just once

--- app/assets/javascripts/get_item_location.js
+++ app/assets/javascripts/get_item_location.js
@@ -1,9 +1,14 @@
-$.ajax({
-  type: 'GET',
-  url: '/items/get_item_location',
-  success: function(json) {
-    json.forEach(function(item) {
-          window.alert(item.name + " is near you!");
-    });
-  }
-});
+function getItemLocation(){
+  $.ajax({
+    type: 'GET',
+    url: '/items/get_item_location',
+    success: function(json) {
+      json.forEach(function(item) {
+          counter += 1;
+          if (counter === 1){
+              alert("Don't forget to pick up: " + item.name)
+          }
+      });
+    }
+  });
+};
