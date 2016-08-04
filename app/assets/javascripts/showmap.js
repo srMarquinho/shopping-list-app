eb9b85b8440d8f36731ed7327b368cf98440e649
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-08-04 13:09:40 +0100

    :guardsman: refactoring and seperating view from js

--- app/assets/javascripts/showmap.js
+++ app/assets/javascripts/showmap.js
@@ -7,26 +7,30 @@ function initMap() {
       var myLocation = new google.maps.LatLng(myLat, myLng);
       var directionsDisplay = new google.maps.DirectionsRenderer(document.getElementById('direction-display'));
       var directionsService = new google.maps.DirectionsService(document.getElementById('direction-service'));
+
+      // Creating Maps
       var mapNoDirections = new google.maps.Map(document.getElementById('no-direction-map'), {
-          center: {
-            lat: itemLatitude,
-            lng: itemLongitude
-          },
+          center: { lat: itemLatitude, lng: itemLongitude },
           zoom: 15,
         });
       var mapDirections = new google.maps.Map(document.getElementById('show-map'), {
-        center: {
-          lat: itemLatitude,
-          lng: itemLongitude
-        },
+        center: { lat: itemLatitude, lng: itemLongitude },
         zoom: 15,
       });
 
+
+      // Setting Icons
       var youIcon = 'https://s31.postimg.org/m9bv4fq7f/you_icon.png'
       var itemIcon = 'https://s32.postimg.org/loajgnlg5/item_icon.png'
 
+      // Creating & Setting Markers
+
       var myMarker = new google.maps.Marker({ map: mapDirections, icon: youIcon });
       var myMarker2 = new google.maps.Marker({ map: mapNoDirections, icon: youIcon });
+      myMarker.setPosition(myLocation);
+      myMarker2.setPosition(myLocation);
+
+      // Creating Item Markers
 
       var itemMarker = new google.maps.Marker({
         position: { lat: itemLatitude, lng: itemLongitude },
@@ -39,6 +43,8 @@ function initMap() {
         icon: itemIcon
       });
 
+
+    // Setting up Display Services
     directionsDisplay.setPanel(document.getElementById('panel'));
       var request = {
         origin: myLocation,
@@ -49,9 +55,9 @@ function initMap() {
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);
       }
-      directionsDisplay.setMap(mapDirections);
-      myMarker.setPosition(myLocation);
-      myMarker2.setPosition(myLocation);
+    // Setting the map
+    directionsDisplay.setMap(mapDirections);
+
     });
   });
  }
