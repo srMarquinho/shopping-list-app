e8b5a5f36af62ed51cc522add36b795e985f08f7
srMarquinho <sr.marquinho@gmail.com>
2016-08-04 12:52:08 +0100

    add buttons

--- app/assets/javascripts/item_toggle.js
+++ app/assets/javascripts/item_toggle.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".item-check").bind("change", function(){
+  $(".item-check").click(function(){
     $.ajax({
       url: "/items/"+this.id+"/toggle",
       type: "POST"
