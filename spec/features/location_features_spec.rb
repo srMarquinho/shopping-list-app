d2846060a5482ddaf89e0c59a0dd0f2d7571e029
Faisal Mahmood <fahmahmood@gmail.com>
2016-08-04 12:02:07 +0100

    edited show maps

---
+++ spec/features/location_features_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature "Location settings" do
+
+  # context "Items have a location" do
+  #   it "attributes coordinates to each item", js:true do
+  #     signup
+  #     click_link 'Add an item'
+  #     fill_in 'Name', with: "Makers Academy"
+  #     fill_in 'pac-input', with: "50 Commercial Street, London, UK"
+  #     click_button 'Create Item'
+  #     click_link 'Makers Academy'
+  #     expect(page).to have_content("50 Commercial Street, London, UK")
+  #   end
+  # end
+end
