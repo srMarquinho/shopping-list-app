c8f4834f7ce54b4be9c550d339bd5b4c34fba38f
srMarquinho <sr.marquinho@gmail.com>
2016-11-01 15:17:52 +0000

    clean spec files

--- spec/features/items_features_spec.rb
+++ spec/features/items_features_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'Items' do
+
   context 'no items have been added' do
+
     scenario 'should display a prompt to add an item' do
       signup
       expect(page).to have_content 'No items yet'
@@ -20,6 +22,7 @@ feature 'Items' do
   end
 
   context 'creating items' do
+
     scenario 'prompts user to fill out a form, then displays the new item' do
       signup
       add_item
@@ -31,57 +34,43 @@ feature 'Items' do
       signup
       add_item(name: 'X')
       expect(page).not_to have_content('X')
-      # expect(page).to have_content('error')
     end
-
-    # scenario 'has a location when added by google maps' do
-    #   signup
-    #   click_link 'add_item'
-    #   fill_in 'Name', with: 'Shampoo'
-    #   fill_in 'pac-input', with: '50 Commer, London, United Kingdom'
-    #   click_button 'Create Item'
-    #   click_link 'Shampoo'
-    #   expect(page).to have_content 'Aldgate East Tube'
-    # end
   end
 
   context 'viewing items' do
 
     scenario 'lets a user view a item' do
-     signup
-     add_item
-     click_link 'Shampoo'
-     click_link 'Map'
-     expect(page).to have_content 'Shampoo'
-     expect(page).to have_content 'Show Directions'
-    #  expect(current_path).to eq "/items"
+      signup
+      add_item
+      click_link 'Shampoo'
+      click_link 'Map'
+      expect(page).to have_content 'Shampoo'
+      expect(page).to have_content 'Show Directions'
     end
   end
 
   context 'editing items' do
 
     scenario 'let a user edit an item' do
-     signup
-     add_item
-     click_link 'Edit'
-     fill_in 'Name', with: 'Shampoo'
-     fill_in 'Description', with: 'Herbal essences'
-     click_button 'Update Item'
-     expect(page).to have_content 'Shampoo'
-     expect(page).to have_content 'Herbal essences'
-     expect(current_path).to eq '/items'
+      signup
+      add_item
+      click_link 'Edit'
+      fill_in 'Name', with: 'Shampoo'
+      fill_in 'Description', with: 'Herbal essences'
+      click_button 'Update Item'
+      expect(page).to have_content 'Shampoo'
+      expect(page).to have_content 'Herbal essences'
+      expect(current_path).to eq '/items'
     end
 
-    scenario 'do not let not owner user edit a restaurant' do
+    scenario 'do not let not owner user edit a item' do
       signup
       add_item
       signout
       signup(email: 'test1@email.com')
       expect(page).not_to have_content 'Shampoo'
     end
-
   end
-
 
   context 'deleting items' do
 
@@ -92,14 +81,12 @@ feature 'Items' do
       expect(page).not_to have_content 'Shampoo'
     end
 
-    scenario 'do not let not owner user delete a restaurant' do
+    scenario 'do not let not owner user delete a item' do
       signup
       add_item
       signout
       signup(email: 'test1@email.com')
       expect(page).not_to have_content 'Shampoo'
    end
-
   end
-
 end
--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 feature "User can sign in and out" do
 
   context "user not signed in and on the homepage" do
+
     it "should see a 'sign in' link and a 'sign up' link" do
       visit('/')
-      # expect(page).to have_link('Sign in')
+      expect(page).to have_button('Sign in')
       expect(page).to have_link('Sign up')
     end
 
@@ -16,7 +17,6 @@ feature "User can sign in and out" do
   end
 
   context "user signed in on the homepage" do
-
     before do
       signup
     end
